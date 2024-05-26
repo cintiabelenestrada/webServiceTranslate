6cf72b93ea30a7028a0ac25ca2b0120022934787
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-26 18:40:18 -0300

    routing bien hecho del componente direccion-ip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DireccionIPComponent } from "./components/direccion-ip/direccion-ip.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DireccionIPComponent]
 })
 export class AppComponent {
   title = 'webServiceTranslate';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DireccionIPComponent } from './components/direccion-ip/direccion-ip.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'ip', component: DireccionIPComponent }
+];
